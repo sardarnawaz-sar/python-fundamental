6ee09e838f71d8cb123b69555c6f8db1fa670736
sardar nawaz <sardarnawaz0626@gmail.com>
2025-08-21 19:54:39 +0500

    python fandamental.py

---
+++ python fundamental.py
@@ -0,0 +1,35 @@
+#arithmetic operators
+# a = 5
+# b = 2
+# sum = a + b
+# print("sum")
+# print(sum)
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+#relaional operators
+# a = 50
+# b = 20
+# print(a != b)
+# print(a <= b)
+# print(a >= b)
+#assigment operators
+# a = 50
+# b = 30
+# num = 10
+# num = num + 30
+# print(num)
+# num += 30
+#logical operators
+# print(not False)
+# print(not True)
+# a = 60
+# b = 32
+# print(not (a<b))
+# val1 = True
+# val2 = True
+# print("and operator", val1 and val2)
+# print("or operator", val1 or val2)
