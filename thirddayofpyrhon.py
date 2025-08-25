8bd8486fdd8c4dd0542d5ed6f9eb73fa11b50e02
sardar nawaz <sardarnawaz0626@gmail.com>
2025-08-25 16:30:12 +0500

    thirddayofpython

---
+++ thirddayofpyrhon.py
@@ -0,0 +1,92 @@
+# marks1 = 94.5
+# marks2 = 87.5 
+# marks3 = 95.8
+# marks4 = 66.7
+# marks5 = 45.9
+# marks = [94.5,95.8,87.5,66.7,45.9]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[3])
+# print(len(marks))
+# student = ["karan", 95.4, 17]
+# print(student)
+
+#we cannot do the this thing in string
+# str ="hello"
+# print(str[0])
+# str(0) = "y"
+# we can do the same thing in list
+# student = ["karan",95.4,17,"delhi"]
+# student[0] = "arjun"
+# print(student)
+# marks = [67,87,90,87,65,89,60]
+# print(marks[0:4])
+# exactly work like string slicing both for negative also
+# marks = [89,87,78,56,65,78,90]
+# marks.append(76)
+# print(marks)
+#listing, sort as asscending
+# list = [5,6,4,8,3,2,0,6]
+# list.append(9)
+# print(list)
+# print(list.sort(reverse=True))
+# print(list)
+# print(list.sort())
+# print(list)
+#it also work on string
+# list = ["qadir","hasina","salman","saima","sardar","ruhi"]
+# print(list.sort(reverse=True))
+# print(list)
+# print(list.sort())
+# print(list)
+#reversing list
+# print(list.reverse())
+# print(list)
+# list.insert(2,"rafida")
+# print(list)
+# list.remove(6)
+# print(list)
+# list.pop(3)
+# print(list)
+# tup = (78,90,87)
+# print(type(tup))
+# print(tup[0])
+# print(tup[2])
+# tup = (1)
+# print(tup)
+# print(type(tup))
+# tup = (1,2,3,4,5,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6)
+# print(tup.index(3))
+# print(tup.count(6))
+
+#let's practice
+# movies = []
+# mov1 = input("1st movie :")
+# mov2 = input("2nd movie :")
+# mov3 = input("3rd movie :")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+# print(movies)
+# movies = []
+# movies.append(input("1st movie :"))
+# movies.append(input("2nd movie :"))
+# movies.append(input("3rd movie :"))
+# print(movies)
+#WAP to check if a list contains a palindrome of elements.(hint: use copy() method)
+# list1 = [1,2,1]
+# list2 = [1,2,3]
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+# if(copy_list1 == list1):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+# copy_list2 = list2.copy()
+# copy_list2.reverse()
+# if(copy_list2 == list2):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
