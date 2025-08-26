0d4b9c1c3963b1ceb5dd67fca1be4de9bac9e87c
sardar nawaz <sardarnawaz0626@gmail.com>
2025-08-26 17:46:03 +0500

    dayfouroflearningpython

---
+++ fourthdayofpython.py
@@ -0,0 +1,68 @@
+# info = {
+#     "name": "sardar",
+#     "age": 21,
+#     "learning": ["python","c","c++","java"],
+#     "topic": ("dictionary","set"),
+# }
+# # print(info)
+# # print(type(info))
+# print(info["name"])
+# print(info["learning"])
+# info["name"] = "sardar nawaz"
+# info["surname"] = "nawaz"
+# print(info)
+# student = {
+#     "name" : "sardar",
+#     "subjects" : {
+#         "phy" : 97,
+#         "math" : 99,
+#         "chems" : 90,
+
+#     }
+# }
+# print(student)
+# print(student["subjects"])
+# print(list(student.keys()))
+# print(len())
+# print(list(student.keys()))
+# print(list(student.values()))
+# print(list(student.items()))
+# pairs = (list(student.items()))
+# print(pairs[0])
+# print(pairs[1])
+# print(student["name"])
+# print(student.get("name"))
+# print(student.get("name1"))
+# student.update({"city" : "booni"})
+# print(student)
+# # set = {1,2,3,4}
+# set = {1,2,3,"hello","hello","world",3,3,3}
+# print(set)
+# print(type(set))
+# collection = {}
+# sard = set()
+# print(type(sard))
+# print(type(collection))
+# sard.add(1)
+# sard.add(2)
+# sard.add(2)
+# # sard.remove(1)
+# sard.add("sardar")
+# sard.add((1,2,3,))
+# sard.clear()
+# sard = {"python","c","java","javascript"}
+# print(sard.pop())
+# set1 = {1,2,3,4}
+# set2 = {4,5,6,7}
+#print(set1.union(set2))
+# print(set1.intersection(set2))
+# dict = {
+#     "table" : "a piece of furniture",
+#     "cat" : ["a small animal","list of facts & furniture"]
+# }
+# print(dict)
+# classroom = {"python","java","c++","python","javascript","java","python","java","c++","c"}
+# print(len(classroom))
+a = 45
+b = 65
+print(a + b)
